00a2c010fecd995e0b2ac019f16ede99ce776783
Prince_Np <46887030+PrinceNiyonshuti@users.noreply.github.com>
2021-10-22 14:53:09 +0200

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="bg-white">
       <div className="px-12">
         <div className="flex justify-center py-2">
-          <div className="flex font-bold ">&copy; Prince NP</div>
+          <div className="flex font-bold ">&copy; Prince NP </div>
         </div>
       </div>
     </div>
